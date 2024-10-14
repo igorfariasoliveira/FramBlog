b53d22c26efcdffade8f0ecd6a772e15b7a118e9
igorfariasoliveira <igor10farias@gmail.com>
2024-10-14 16:05:24 -0300

    Initial commit

---
+++ src/main/java/com/descomplica/FramBlog/FramBlogApplication.java
@@ -0,0 +1,13 @@
+package com.descomplica.FramBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FramBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/descomplica/FramBlog/FramBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descomplica.FramBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
