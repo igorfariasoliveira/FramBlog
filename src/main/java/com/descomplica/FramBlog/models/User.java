c1716a5fe538bc7e504d383b24a8089a6cf90709
igorfariasoliveira <igor10farias@gmail.com>
2024-10-15 09:53:21 -0300

    Update User.java

--- src/main/java/com/descomplica/FramBlog/models/User.java
+++ src/main/java/com/descomplica/FramBlog/models/User.java
@@ -1,7 +1,24 @@
 package com.descomplica.FramBlog.models;
 
-import jakarta.persistence.Entity;
+import com.descomplica.FramBlog.enums.RoleEnum;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String userName;
+    private String email;
+    private String password;
+    private RoleEnum role;
+
+    public User(final Long userId,final String userName,final String email,final String password,final RoleEnum role) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
