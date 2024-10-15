2a80cddfa4dce667ba55425d8a1bb1ac459e798c
igorfariasoliveira <igor10farias@gmail.com>
2024-10-15 11:19:38 -0300

    Projeto Java

--- src/main/java/com/descomplica/FramBlog/models/User.java
+++ src/main/java/com/descomplica/FramBlog/models/User.java
@@ -21,4 +21,47 @@ public class User {
         this.password = password;
         this.role = role;
     }
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleEnum getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
 }
